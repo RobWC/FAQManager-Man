f1a505b0b8d332221bd254a6f3246e9bc39016f7
Rob Cameron <rwcameron@gmail.com>
2011-10-24 00:02:12 -0700

    removing requirejs as it added 300ms of latencey

--- _attachments/scripts/jquery.couchLogin.js
+++ _attachments/scripts/jquery.couchLogin.js
@@ -49,19 +49,10 @@
         elem.delegate("a[href=#logout]", "click", function() {
             $.couch.logout({success : initWidget});
         });
-    }
-    var templates = {
-        adminParty : '<p><strong>Admin party, everyone is admin!</strong> Fix this in <a href="/_utils/index.html">Futon</a> before proceeding.</p>',
-        loggedOut : '<a href="#login">Login</a>',
-        loginForm : '<form class="login"><label for="name">Username</label> <input type="text" name="name" value="" autocapitalize="off" autocorrect="off"><label for="password">Password</label> <input type="password" name="password" value=""><input type="submit" value="Login">'
     };
     function loggedIn(r) {
-        var auth_db = encodeURIComponent(r.info.authentication_db)
-        , uri_name = encodeURIComponent(r.userCtx.name)
-        , span = $('<span><a target="_new" href="/_utils/document.html?' 
-            + auth_db +'/org.couchdb.user%3A' + uri_name 
-            + '" class="name"></a>! <a href="#logout">Logout</a></span>');
-        $('a.name', span).text(r.userCtx.name); // you can get the user name here
-        return span;
+        var div = $('<span><span id="username">' + r.userCtx.name + '</span> <a href="#logout">Logout</a></span>');
+        //$('span#username').text(r.userCtx.name); // you can get the user name here
+        return div;
     }
 })(jQuery);
--- _attachments/scripts/main.js
+++ _attachments/scripts/main.js
@@ -1,7 +1,6 @@
-require(["jquery-1.6.4.min", "jquery.couch", "jquery.couchLogin","jquery-ui-1.8.16.custom.min", "jquery.flip.min","jquery.multiselect.min","jquery.multiselect.filter.min"], function($) {
-
+require(["jquery-1.6.4.min","jquery-ui-1.8.16.custom.min", "jquery.couch","jquery.multiselect.min","jquery.multiselect.filter.min", "jquery.couchLogin","jquery.flip.min"], function($) {
 });
 
-require([ "common-proto", "common-helper","index"], function($) {
+require(["common-proto", "common-helper","index"], function($) {
 	
 });
